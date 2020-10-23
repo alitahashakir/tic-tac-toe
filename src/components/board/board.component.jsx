dc94ceeafd2798c7edaa5c2c1da316541e466d06
unknown <alitahashakir@gmail.com>
2020-10-24 00:58:42 +0500

    playng around

---
+++ src/components/board/board.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Square from '../square/square.component'
+
+import './board.styles.scss'
+
+const Board = ({squares, onClick}) => {
+    return (
+        <div className="board">
+            Board
+        </div>
+    );
+};
+
+export default Board;
---
+++ src/components/square/square.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './square.styles.scss'
+
+const Square = (props) => {
+    return (
+        <button className="square">
+            {/*TODO*/}
+        </button>
+    );
+};
+
+export default Square;
